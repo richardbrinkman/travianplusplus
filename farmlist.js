4569388f7c95061ec35a2f17cee678d04f0fd2d0
Richard Brinkman <r.brinkman@gmail.com>
2012-10-01 13:53:33 +0200

    FIX: frozen times troops

--- farmlist.js
+++ farmlist.js
@@ -1,30 +1,37 @@
 function dorf1() {
-	var mapdetails = document.getElementById("map_details")
-	if (mapdetails)
-		mapdetails.innerHTML += 
-			"<div class=\"boxes villageList units\">" +
-				"<div class=\"boxes-tl\"></div>"+
-				"<div class=\"boxes-tr\"></div>"+
-				"<div class=\"boxes-tc\"></div>"+
-				"<div class=\"boxes-ml\"></div>"+
-				"<div class=\"boxes-mr\"></div>"+
-				"<div class=\"boxes-mc\"></div>"+
-				"<div class=\"boxes-bl\"></div>"+
-				"<div class=\"boxes-br\"></div>"+
-				"<div class=\"boxes-bc\"></div>"+
-				"<div class=\"boxes-contents cf\">"+
-					"<table id=\"farmlist\" cellpadding=\"1\" cellspacing=\"1\">"+
-						"<thead>"+
-							"<tr style=\"display: table-row; \">"+
-								"<th colspan=\"3\">Farmlist:</th>"+
-							"</tr>"+
-						"</thead>"+
-						"<tbody>"+
-							"<tr colspan=\"3\"><td>Tralala</td></tr>"+
-						"</tbody>"
-					"</table>"
-				"</div>"+
-			"</div>";
+	var mapdetails = document.getElementById("map_details");
+	var clearDiv = document.evaluate(
+		"./div[@class=\"clear\"]",
+		mapdetails,
+		null,
+		XPathResult.ANY_TYPE,
+		null
+	).iterateNext();
+	var farms = document.createElement("div");
+	farms.className = "boxes villageList units";
+	farms.innerHTML = 
+		"<div class=\"boxes-tl\"></div>"+
+		"<div class=\"boxes-tr\"></div>"+
+		"<div class=\"boxes-tc\"></div>"+
+		"<div class=\"boxes-ml\"></div>"+
+		"<div class=\"boxes-mr\"></div>"+
+		"<div class=\"boxes-mc\"></div>"+
+		"<div class=\"boxes-bl\"></div>"+
+		"<div class=\"boxes-br\"></div>"+
+		"<div class=\"boxes-bc\"></div>"+
+		"<div class=\"boxes-contents cf\">"+
+			"<table id=\"farmlist\" cellpadding=\"1\" cellspacing=\"1\">"+
+				"<thead>"+
+					"<tr style=\"display: table-row; \">"+
+						"<th colspan=\"3\">Farmlist:</th>"+
+					"</tr>"+
+				"</thead>"+
+				"<tbody>"+
+					"<tr><td colspan=\"3\">Tralala</td></tr>"+
+				"</tbody>"+
+			"</table>"+
+		"</div>";
+	mapdetails.insertBefore(farms, clearDiv);
 }
 
 function positionDetails() {
