928ae581db56b4c663976266ce41e20057b90e7c
Richard Brinkman <r.brinkman@gmail.com>
2012-10-01 22:47:03 +0200

    ADD: save and retrieval of farm names

--- farmlist.js
+++ farmlist.js
@@ -1,37 +1,81 @@
 function dorf1() {
-	var mapdetails = document.getElementById("map_details");
-	var clearDiv = document.evaluate(
-		"./div[@class=\"clear\"]",
-		mapdetails,
+	chrome.storage.sync.get(null, function(db) {
+		var mapdetails = document.getElementById("map_details");
+		var clearDiv = document.evaluate(
+			"./div[@class=\"clear\"]",
+			mapdetails,
+			null,
+			XPathResult.ANY_TYPE,
+			null
+		).iterateNext();
+		var farms = document.createElement("div");
+		farms.className = "boxes villageList units";
+		var html =
+			"<div class=\"boxes-tl\"></div>"+
+			"<div class=\"boxes-tr\"></div>"+
+			"<div class=\"boxes-tc\"></div>"+
+			"<div class=\"boxes-ml\"></div>"+
+			"<div class=\"boxes-mr\"></div>"+
+			"<div class=\"boxes-mc\"></div>"+
+			"<div class=\"boxes-bl\"></div>"+
+			"<div class=\"boxes-br\"></div>"+
+			"<div class=\"boxes-bc\"></div>"+
+			"<div class=\"boxes-contents cf\">"+
+				"<table id=\"farmlist\" cellpadding=\"1\" cellspacing=\"1\">"+
+					"<thead>"+
+						"<tr style=\"display: table-row; \">"+
+							"<th colspan=\"3\">Farmlist:</th>"+
+						"</tr>"+
+					"</thead>"+
+					"<tbody>";
+		if (db.farms)
+			for (i=0; i<db.farms.length; i++) {
+				alert("Before: " + html);
+				html +=
+					"<tr><td colspan=\"3\">" + db.farms[i].villageName + "</td></tr>";
+				alert("After: " + html);
+			}
+		html +=
+					"</tbody>"+
+				"</table>"+
+			"</div>";
+		farms.innerHTML = html;
+		mapdetails.insertBefore(farms, clearDiv);
+	});
+}
+
+function addToFarmList() {
+	var coordinates = document.evaluate(
+		"//div[@id=\"content\"]/h1/span[1]",
+		document,
 		null,
 		XPathResult.ANY_TYPE,
 		null
 	).iterateNext();
-	var farms = document.createElement("div");
-	farms.className = "boxes villageList units";
-	farms.innerHTML = 
-		"<div class=\"boxes-tl\"></div>"+
-		"<div class=\"boxes-tr\"></div>"+
-		"<div class=\"boxes-tc\"></div>"+
-		"<div class=\"boxes-ml\"></div>"+
-		"<div class=\"boxes-mr\"></div>"+
-		"<div class=\"boxes-mc\"></div>"+
-		"<div class=\"boxes-bl\"></div>"+
-		"<div class=\"boxes-br\"></div>"+
-		"<div class=\"boxes-bc\"></div>"+
-		"<div class=\"boxes-contents cf\">"+
-			"<table id=\"farmlist\" cellpadding=\"1\" cellspacing=\"1\">"+
-				"<thead>"+
-					"<tr style=\"display: table-row; \">"+
-						"<th colspan=\"3\">Farmlist:</th>"+
-					"</tr>"+
-				"</thead>"+
-				"<tbody>"+
-					"<tr><td colspan=\"3\">Tralala</td></tr>"+
-				"</tbody>"+
-			"</table>"+
-		"</div>";
-	mapdetails.insertBefore(farms, clearDiv);
+	var farm = {
+		villageName: document.evaluate(
+			"./span[@class=\"coordText\"]", 
+			coordinates, null, XPathResult.ANY_TYPE, null
+		).iterateNext().innerText,
+		x: document.evaluate(
+			"./span[@class=\"coordinatesWrapper\"]/span[@class=\"coordinateX\"]",
+			coordinates, null, XPathResult.ANY_TYPE, null
+		).iterateNext().innerText.slice(1),
+		y: document.evaluate(
+			"./span[@class=\"coordinatesWrapper\"]/span[@class=\"coordinateY\"]",
+			coordinates, null, XPathResult.ANY_TYPE, null
+		).iterateNext().innerText.replace(")", "")
+	}
+	chrome.storage.sync.get(null, function(db) {
+		if (!db)
+			db = {};
+		if (!db.farms)
+			db.farms = [];
+		db.farms.push(farm); //TODO: check if farm already exists
+		chrome.storage.sync.set(db, function() {
+			alert("Toegevoegd: "+ farm.villageName + " op " + farm.x + "," + farm.y);
+		});
+	});
 }
 
 function positionDetails() {
@@ -40,14 +84,16 @@ function positionDetails() {
 		var detailImage = tileDetails.getElementsByTagName("div");
 		if (detailImage.length>0) {
 			var options = detailImage[0].getElementsByTagName("div");
-			if (options.length>0)
+			if (options.length>0) {
 				options[0].innerHTML += 
 					"<div class=\"option\">"+
-						"<a onclick=\"addToFarmList()\" class=\"a arrow\" title=\"Voeg aan farmlijst toe\">Voeg aan farmlijst toe</a>"+
+						"<a id=\"addToFarmListLink\" class=\"a arrow\" title=\"Voeg aan farmlijst toe\">Voeg aan farmlijst toe</a>"+
 					"</div>";
+				document.getElementById("addToFarmListLink").onclick = addToFarmList;
+			}
 		}
 	} else
-		window.alert("Geen tileDetails gevonden");
+	window.alert("Geen tileDetails gevonden");
 }
 
 var uri = document.documentURI;
